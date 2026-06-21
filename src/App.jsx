7e2a8e15d3b29a83ac4388761ef9c052895f8b8b
dimkungz <dimkungz@gmail.com>
2026-06-21 15:35:59 +0700

    feat: create new project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section>
+        <h1>My First React App on Vite</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
